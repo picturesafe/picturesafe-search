1b3047520998b84bdff2e9ed0ff84568f1a3e453
Jörg Frommann <jfrommann@picturesafe.de>
2020-04-20 17:09:11 +0200

    Test load by extracted ID

--- src/test/java/de/picturesafe/search/elasticsearch/ElasticsearchServiceWithoutFieldDefinitionsIT.java
+++ src/test/java/de/picturesafe/search/elasticsearch/ElasticsearchServiceWithoutFieldDefinitionsIT.java
@@ -40,7 +40,6 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -142,14 +141,13 @@ public class ElasticsearchServiceWithoutFieldDefinitionsIT extends AbstractElast
         assertEquals(3, result.getTotalHitCount());
         assertEquals(3, result.getResultCount());
 
-        result.getSearchResultItems().forEach(i -> {
-            LOGGER.debug("{}", i);
-            assertNotNull(i.getId());
-        });
-
-        final List<String> names = result.getSearchResultItems().stream().map(i -> (String) i.getAttribute("name")).collect(Collectors.toList());
-        assertEquals("name-1", names.get(0));
-        assertEquals("name-2", names.get(1));
-        assertEquals("name-3", names.get(2));
+        int idx = 0;
+        for (final SearchResultItem item : result.getSearchResultItems()) {
+            LOGGER.debug("{}", item);
+            assertEquals("name-" + (idx + 1), item.getAttribute("name"));
+            assertNotNull(item.getId());
+            assertEquals("name-" + (idx + 1), elasticsearchService.getDocument(indexAlias, item.getId()).get("name"));
+            idx++;
+        };
     }
 }
