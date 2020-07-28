66e3e5c00ffa4b7826c81ae71c28bca50b0ac7e0
Jörg Frommann <jfrommann@picturesafe.de>
2020-07-28 11:33:58 +0200

    Implemented resolving simple fields

--- src/main/java/de/picturesafe/search/elasticsearch/impl/mapping/MappingResolver.java
+++ src/main/java/de/picturesafe/search/elasticsearch/impl/mapping/MappingResolver.java
@@ -17,9 +17,13 @@
 package de.picturesafe.search.elasticsearch.impl.mapping;
 
 import de.picturesafe.search.elasticsearch.config.FieldConfiguration;
+import de.picturesafe.search.elasticsearch.config.impl.StandardFieldConfiguration;
+import de.picturesafe.search.elasticsearch.config.impl.SuggestFieldConfiguration;
+import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.NotImplementedException;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
@@ -33,7 +37,7 @@ public class MappingResolver {
         final List<FieldConfiguration> fieldConfigurations = new ArrayList<>();
         final Set<Locale> locales = new HashSet<>();
 
-        final Map<String, Object> properties = properties(mapping, indexName, "mappings", "properties");
+        final Map<String, Object> properties = mappingObject(mapping, indexName, "mappings", "properties");
         properties.forEach((name, value) -> {
             final MappingField field = resolveField(name, (Map<String, Object>) value);
             fieldConfigurations.add(field.getFieldConfiguration());
@@ -43,15 +47,51 @@ public class MappingResolver {
     }
 
     private static MappingField resolveField(String name, Map<String, Object> properties) {
+        final String type = (String) properties.get("type");
+        final FieldConfiguration fieldConfiguration;
+
+        if (type == null) {
+            return resolveMultiField(name, properties);
+        } else if (type.equals("nested")) {
+            return resolveNestedField(name, properties);
+        } else if (type.equals("completion")) {
+            fieldConfiguration = SuggestFieldConfiguration.name(name);
+        } else {
+            final boolean sortableAndAggregatable = isSortableAndAggregatable(properties);
+            fieldConfiguration = StandardFieldConfiguration.builder(name, type)
+                    .sortable(sortableAndAggregatable)
+                    .aggregatable(sortableAndAggregatable)
+                    .build();
+        }
+        return new MappingField(fieldConfiguration, Collections.emptyList());
+    }
+
+    private static MappingField resolveMultiField(String name, Map<String, Object> properties) {
         throw new NotImplementedException("Not yet implemented!"); // ToDo
     }
 
-    @SuppressWarnings("unchecked")
-    private static Map<String, Object> properties(Map<String, Object> doc, String... names) {
-        Map<String, Object> props = doc;
-        for (final String name : names) {
-            props = (Map<String, Object>) props.get(name);
+    private static MappingField resolveNestedField(String name, Map<String, Object> properties) {
+        throw new NotImplementedException("Not yet implemented!"); // ToDo
+    }
+
+    private static boolean isSortableAndAggregatable(Map<String, Object> properties) {
+        if (!properties.get("type").equals("text")) {
+            return true;
         }
-        return props;
+
+        final Map<String, Object> fields = mappingObject(properties, "fields");
+        return MapUtils.isNotEmpty(fields) && fields.containsKey("keyword");
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Map<String, Object> mappingObject(Map<String, Object> doc, String... names) {
+        Map<String, Object> obj = doc;
+        for (final String name : names) {
+            obj = (Map<String, Object>) obj.get(name);
+            if (obj == null) {
+                break;
+            }
+        }
+        return obj;
     }
 }
